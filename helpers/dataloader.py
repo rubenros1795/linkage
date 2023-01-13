9eda90f2d886beae14ecbf009a1ee7b490a74783
Ruben <rb@pop-os.localdomain>
2023-01-13 13:08:02 +0100

    notebook for lda linkage comparisons, new dataloader

--- helpers/dataloader.py
+++ helpers/dataloader.py
@@ -1,73 +1,80 @@
 import pandas as pd
-from top2vec import Top2Vec
 from pathlib import Path
 import yaml
 
 cf = yaml.safe_load(Path('config.yml').read_text())
 
-def load(cf=cf,lmodel=False,ldata=True,llabels=True,lwords=True,ldists=True,ltranslator=True):
-    opd = dict()
-    # Load model
-    if lmodel == True:
-        opd['model'] = Top2Vec.load(cf['model_path'])
-
-    # Load Data
-    if ldata == True:
-        data = pd.read_csv(cf['data_path'],sep='\t',usecols=['date','speech_id','role','member-ref'])
-        data['date'] = pd.to_datetime(data.date,infer_datetime_format=True)
-        opd['data'] = data
-
-    # Model Labels (to prune non-semantic, procedural and rhetorical topics)
-    if llabels == True or lwords == True:
-        keys = pd.read_csv(cf['keys_path'],sep='\t')
-        labels = dict(zip(keys.topic_index,keys.label))
-        words = dict(zip(keys.topic_index,keys.words))
-        opd['labels'] = labels
-        opd['words'] = words
-
-    # Distributions (prune irrelevant topics)
-    if ldists == True:
-        dists = pd.read_csv(cf['dist_path_raw'],sep='\t',header=None)
-        dists = dists[[t for t,l in labels.items() if l not in ['nonsem','rhetoric','proc']]]
-        dists = dists.set_index(data.date)
-        dists = dists + dists.min().abs()
-        dists = dists.div(dists.sum(axis=1), axis=0)
-        # Drop Chairs
-        nonchair_indices = data[data.role != 'chair'].index
-        dists = dists.reset_index().loc[nonchair_indices].set_index('date')
-        opd['dists'] = dists
-
-    # Create a dictionary with the indices of the semantic topics (for converting between pandas / numpy matrices)
-    if ltranslator == True:
-        sem_col_translator = {c:int(k) for c,k in enumerate(dists.columns)}
-        opd['translator'] = sem_col_translator
-
-    return opd
-
-def load_lda(cf=cf,agg_level="speech",ldata=True,lwords=True,ldists=True):
+def load_lda(cf=cf,agg_level="speech",remove_labels=[]):
     """
     cf: config file (yaml)
     agg_level: model type, aggregated on "speech" or "day"
-    ldata: Bool, whether to load data
-    lwords: Bool, whether to load topic keys
-    ldists: Bool, whether to load topic-doc distributions
     """
 
     opd = dict()
-    if ldata == True:
-        data = pd.read_csv(cf[f'data_path_{agg_level}'],sep='\t')
-        data['date'] = pd.to_datetime(data.date,infer_datetime_format=True)
-        opd['data'] = data
-    if lwords == True:
-        keys = pd.read_csv(cf[f'lda_keys_path_{agg_level}'],sep='\t',header=None)
-        keys.columns = ['i','size','words']
-        words = dict(zip(keys.i,keys.words))
-        opd['words'] = words
-    if ldists == True:
-        dists = pd.read_csv(cf[f'lda_dist_path_{agg_level}'],sep='\t',header=None).iloc[:,2:]
-        dists.columns = range(len(dists.columns))
-        dists = dists.set_index(data.date)
-        dists = dists + dists.min().abs()
-        dists = dists.div(dists.sum(axis=1), axis=0)
-        opd['dists'] = dists
+
+    # Load data
+    data = pd.read_csv(cf[f'data_path_{agg_level}'],sep='\t')
+    data['date'] = pd.to_datetime(data.date,infer_datetime_format=True)
+    opd['data'] = data
+
+    # Load keys and labels
+    keys = pd.read_csv(cf[f'lda_keys_path_{agg_level}'],sep='\t')
+    words = dict(zip(keys.index,keys.words))
+    labels = dict(zip(keys.index,keys.label))
+    opd['words'] = words
+    opd['labels'] = labels
+
+    # Load dists
+    dists = pd.read_csv(cf[f'lda_dist_path_{agg_level}'],sep='\t',header=None).iloc[:,2:]
+    dists.columns = range(len(dists.columns))
+
+    if len(remove_labels) != 0:
+        dists = dists[[t for t,l in labels.items() if l not in remove_labels]]
+        sem_col_translator = {c:int(k) for c,k in enumerate(dists.columns)}
+        opd['translator'] = sem_col_translator
+
+    dists = dists.set_index(data.date)
+    dists = dists + dists.min().abs()
+    dists = dists.div(dists.sum(axis=1), axis=0)
+    opd['dists'] = dists
+
+    return opd
+
+def load_top2vec(cf=cf,remove_labels=[]):
+    """
+    cf: config file (yaml)
+    agg_level: model type, aggregated on "speech" or "day"
+    """
+
+    opd = dict()
+
+    # Load data
+    data = pd.read_csv(cf[f't2v_data_path'],sep='\t')
+    data['date'] = pd.to_datetime(data.date,infer_datetime_format=True)
+    opd['data'] = data
+
+    # Load keys and labels
+    keys = pd.read_csv(cf[f't2v_keys_path'],sep='\t')
+    words = dict(zip(keys.index,keys.words))
+    labels = dict(zip(keys.index,keys.label))
+    opd['words'] = words
+    opd['labels'] = labels
+
+    # Load dists
+    dists = pd.read_csv(cf[f't2v_dist_path_raw'],sep='\t',header=None)
+
+    if len(remove_labels) != 0:
+        dists = dists[[t for t,l in labels.items() if l not in remove_labels]]
+        sem_col_translator = {c:int(k) for c,k in enumerate(dists.columns)}
+        opd['translator'] = sem_col_translator
+
+    dists = dists.set_index(data.date)
+    dists = dists + dists.min().abs()
+    dists = dists.div(dists.sum(axis=1), axis=0)
+
+    # Drop Chairs (for old models where chairs are included in model, in new models, chairs are dropped)
+    nonchair_indices = data[data.role != 'chair'].index
+    dists = dists.reset_index().loc[nonchair_indices].set_index('date')
+    opd['dists'] = dists
+
     return opd
